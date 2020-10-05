cb6f06ecbc9e1cb4af415629135fa2c982fa17dc
Илья Грунов <ilya.grunov@gmail.com>
2020-10-05 15:46:35 +0300

    Добавлена маршрутизация приложения.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,46 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import MainScreen from "../main-screen/main-screen";
+import AddReview from "../add-review/add-review";
+import Film from "../film/film";
+import MyList from "../my-list/my-list";
+import Player from "../player/player";
+import SignIn from "../sign-in/sign-in";
 
 const App = ({name, genre, release}) => {
   return (
-    <MainScreen
-      name = {name}
-      genre = {genre}
-      release = {release}
-    />
+    // <MainScreen
+    //   name = {name}
+    //   genre = {genre}
+    //   release = {release}
+    // />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainScreen
+            name = {name}
+            genre = {genre}
+            release = {release}
+          />
+        </Route>
+        <Route exact path="/login">
+          <SignIn />
+        </Route>
+        <Route exact path="/mylist">
+          <MyList />
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReview />
+        </Route>
+        <Route exact path="/films/:id">
+          <Film />
+        </Route>
+        <Route exact path="/player/:id">
+          <Player />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
